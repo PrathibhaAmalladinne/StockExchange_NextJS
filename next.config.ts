c67bc7b0a7b014d21b20c8a2feed2c943eb179b5
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-11-26 17:39:43 +0530

    build errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+    VERCEL_URL: process.env.VERCEL_URL,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
